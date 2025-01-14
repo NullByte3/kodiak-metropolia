096aee7dd3f0212073a02b10ee95a04c174da50b
vivsam <hi@coolkidz.me>
2025-01-14 09:32:12 +0200

    multi lines

--- src/club/nullbyte3/kodiak/module1/HelloWorld.java
+++ src/club/nullbyte3/kodiak/module1/HelloWorld.java
@@ -2,6 +2,12 @@ package club.nullbyte3.kodiak.module1;
 
 public class HelloWorld {
 
+    // single line comment
+    /*
+    Multiple line comment
+    i guess, doesn't really matter.
+     */
+
     public static void main(String[] args) {
         System.out.println("Hello, World!");
 
